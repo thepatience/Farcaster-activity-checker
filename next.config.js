2dd52df04f97164fa2cd9c681028677d157a671c
Thepatience <127692196+thepatience@users.noreply.github.com>
2025-11-14 10:12:13 +0700

    Refactor next.config.js for improved readability

--- next.config.js
+++ next.config.js
@@ -1,1 +1,6 @@
-module.exports = { reactStrictMode: true, experimental: { appDir: true } };
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
